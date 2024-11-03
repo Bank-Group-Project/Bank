4020e308004d11f644cc8141756d6c0769d13e03
George Sladkovsky <102670724+GeorgiyGit@users.noreply.github.com>
2024-11-03 14:23:57 +0100

    Revert "BGP-29 Add base Bank Account Service"

--- Bank.AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.ApiGateway>("apigateway");
-
-builder.AddProject<Projects.BankAccountService_Api>("bankaccountservice-api");
-
-builder.Build().Run();
