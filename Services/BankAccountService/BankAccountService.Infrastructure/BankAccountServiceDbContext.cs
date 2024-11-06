1159c8d3a4820cc218f9c6da248e056b45a190ca
George Sladkovsky <GeorgiyGitHub@gmail.com>
2024-11-06 21:37:11 +0100

    feat: add db context in BA service [BGP-33]

---
+++ Services/BankAccountService/BankAccountService.Infrastructure/BankAccountServiceDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BankAccountService.Core.Entities.BankAccountEntities;
+using BankAccountService.Core.Entities.CustomerEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountService.Infrastructure
+{
+    public class BankAccountServiceDbContext : DbContext
+    {
+        public virtual DbSet<BankAccount> BankAccounts { get; set; }
+        public virtual DbSet<BankAccountStatus> BankAccountStatuses { get; set; }
+        public virtual DbSet<BankAccountType> BankAccountTypes { get; set; }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public BankAccountServiceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
