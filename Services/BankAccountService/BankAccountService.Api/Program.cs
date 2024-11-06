b494d50910009554eb7719f8fc01ad7a47144a55
George Sladkovsky <GeorgiyGitHub@gmail.com>
2024-11-06 22:32:50 +0100

    feat: configure connection to BA service db [BGP-33]

--- Services/BankAccountService/BankAccountService.Api/Program.cs
+++ Services/BankAccountService/BankAccountService.Api/Program.cs
@@ -1,8 +1,20 @@
+using BankAccountService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("TestDb") ?? throw new InvalidOperationException("Connection string 'TestDb' not found.");
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddDbContext<BankAccountServiceDbContext>(x => x.UseSqlServer(connectionString, builder =>
+{
+    builder.EnableRetryOnFailure(1, TimeSpan.FromSeconds(5), null);
+}));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
