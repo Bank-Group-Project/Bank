a8d426722b8d6c5ddbd25cb4d1099e22fe49e558
George Sladkovsky <GeorgiyGitHub@gmail.com>
2024-11-08 14:16:15 +0100

    feat: add pre-version of DTOs [BGP-68]

---
+++ Services/BankAccountService/BankAccountService.Core/DTOs/BankAccounts/BankAccountTypeDTOs/Responses/SimpleBankAccountTypeResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountService.Core.DTOs.BankAccounts.BankAccountTypeDTOs.Responses
+{
+    public class SimpleBankAccountTypeResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
